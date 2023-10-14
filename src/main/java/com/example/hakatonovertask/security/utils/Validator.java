3d9c1e18d4fa9a5600e8d9f9d3285bec6a595cb0
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-10-14 20:07:25 +0300

    Made frame for validation

---
+++ src/main/java/com/example/hakatonovertask/security/utils/Validator.java
@@ -0,0 +1,10 @@
+package com.example.hakatonovertask.security.utils;
+
+import com.example.hakatonovertask.security.model.EnroleeModel;
+
+public class Validator {
+    public static boolean validate(EnroleeModel enroleeModel) {
+        return true;
+        //TODO
+    }
+}
