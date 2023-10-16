469277423c814eca9af5833f9205383e6138faf8
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-10-16 15:01:44 +0300

    Changed jpa settings of DB

--- src/main/java/com/example/hakatonovertask/security/utils/Roles.java
+++ src/main/java/com/example/hakatonovertask/security/utils/Roles.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 @Getter
 @ToString
 public enum Roles implements GrantedAuthority {
-    ENROLLEE("EnrollEe"),
+    ENROLLEE("Enrollee"),
     TEACHER("Teacher"),
     STUDENT("Student"),
     GUEST("Guest"),
