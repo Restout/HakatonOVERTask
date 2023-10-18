9d3625df8626b61e5a8b8df3c7dfda4ce4672fe9
ThreeAi <alexandrovaa2003@gmail.com>
2023-10-19 01:32:55 +0300

    delete: duplicated repository

--- src/main/java/com/example/hakatonovertask/repositories/EnrolleeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hakatonovertask.repositories;
-
-import com.example.hakatonovertask.models.Enrollee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrolleeRepository extends JpaRepository<Enrollee, Integer> {
-}
